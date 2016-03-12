b1d691d739b6afcf8a83e1b645017aba315440a0
Jose Deschamps <deschamps.jose001@gmail.com>
2016-03-12 12:44:05 -0500

    Forest Defense HW5
    This is a ambitious build.

---
+++ Hostos/GD205/GD205HW5/Assets/Scripts/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+
+	public GameObject bullet;
+	public float spinSpeed;
+	public GameObject towerHead;
+	public AudioClip bulletSound;
+	[HideInInspector][SerializeField] new AudioSource audio;
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		towerHead.transform.Rotate (Vector3.up * Time.deltaTime *spinSpeed,Space.World);  // used to rotation my towerhead for a cool effects! Juice!
+
+
+	}
+
+	void OnTriggerEnter(Collider co){    //check to see if my enemymoving script is near the range of the collider and if
+		                                           //so shoot at it.
+
+		if(co.GetComponent<enemyMove>()){
+
+			GameObject check = (GameObject)Instantiate (bullet, transform.position, Quaternion.identity);
+			check.GetComponent<MoveBullet> ().target = co.transform;
+			audio.PlayOneShot(bulletSound);
+
+	}
+}
+}
---
+++ Hostos/GD205/GD205HW5/Assets/Scripts/enemyMove.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyMove : MonoBehaviour {
+	
+	public AudioClip[] audioClips;
+	public Transform[] pathPoints;
+	public float speed = 9f;
+	public int waypoint = 0;
+	Transform targetWayPoint;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//enemies checking to see where to move toward.
+		if (waypoint < this.pathPoints.Length) {
+
+			if (targetWayPoint == null)
+				targetWayPoint = pathPoints [waypoint];
+			move ();
+
+
+			}
+		}
+
+
+
+
+	void move(){
+		//Move to the toward to my pathPoints.
+		transform.position = Vector3.MoveTowards (transform.position, targetWayPoint.position, speed * Time.deltaTime);
+
+		if (transform.position == targetWayPoint.position) {  //check my waypoint position.
+
+			waypoint++;
+			targetWayPoint = pathPoints [waypoint];  //this make the enemy follow the path using my array.
+		}
+
+		//Destroy the enemies when they reach the end path.
+		if (waypoint == 6) {
+
+			Destroy (this.gameObject);
+		}
+	}
+
+		
+
+
+
+	}
+
+
