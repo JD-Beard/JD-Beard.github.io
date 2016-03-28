e89695ca55bd664e8d52c9b66f6cc9da125b7c33
Jose Deschamps <deschamps.jose001@gmail.com>
2016-03-27 22:23:44 -0400

    GD205HW6 Destroy
    This is a class assignment.

---
+++ Hostos/GD205/GD205HW6/Assets/Scripts/Movement.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public GameObject enemy;
+	public GameObject newEnemy;
+	public Vector3 location;
+	public float followSpeed;
+	Vector3 nearby;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+
+		location = enemy.transform.position;
+		transform.LookAt (location);
+
+		if (Vector3.Distance (location, transform.position) > 1f) {
+
+			GetComponent<Rigidbody> ().AddForce (Vector3.Normalize (location - transform.position) * followSpeed);
+
+		} else {
+			GetComponent<Rigidbody> ().velocity = Vector3.zero;
+			///GameObject temp = enemy;
+
+			//MakeSpawn ();
+			//Destroy (temp);
+
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			GameObject temp = enemy;
+			MakeSpawn ();
+			Destroy (temp);
+
+		}
+
+
+
+
+
+	}
+
+
+	
+	void MakeSpawn(){
+
+			nearby = location + Random.insideUnitSphere * 2.5f;
+			GameObject clone = Instantiate( newEnemy, nearby, Quaternion.identity) as GameObject;
+
+			enemy = clone;
+
+		}
+
+
+		
+
+
+
+
+	}
+
---
+++ Hostos/GD205/GD205HW6/Assets/Scripts/Spark.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spark : MonoBehaviour {
+
+
+	public GameObject pop;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+
+		if (Input.GetMouseButtonDown (0)) {
+
+			Instantiate (pop, transform.position, Quaternion.identity);
+
+
+		}
+
+
+
+
+	}
+
+
+
+
+
+
+
+        
+}
