7afa34c6bd57cee0223705bdd5178d674947d313
Jose Deschamps <deschamps.jose001@gmail.com>
2016-04-25 09:13:43 -0400

    Final Project

---
+++ Hostos/GD205/GD205Final/BoneRunner/Assets/Scripts/TileManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+	//public GameObject leftTilePrefab;
+	public GameObject topTilePrefab;
+	public GameObject currentTile;
+
+	// Use this for initialization
+	void Start () {
+	
+		for (int i = 0; i < 10; i++) {
+
+			SpawnTile ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void SpawnTile(){
+
+		currentTile = (GameObject)Instantiate (topTilePrefab, currentTile.transform.GetChild(1).position, Quaternion.identity);
+
+	}
+}
